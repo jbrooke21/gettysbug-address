5b45a5178db2586cee1ed2829a08f211af11c37f
jbrooke21 <jessica.brooke@symphony.com>
2016-05-17 19:11:03 -0700

    updates

--- gettysburg-address/app.js
+++ gettysburg-address/app.js
@@ -1,43 +1,43 @@
 $(document).ready(function() {
 
+	//Fade in on load
 	$(document).ready(function() {
   		$("body").fadeIn(5000);
 	});
 
+	//On click, change text to large
 	$(document).on("click", "#switcher-large", function() {
 		$(".speech").css("font-size", "16px");
 	});
-
+	//On click change text to small
 	$(document).on("click", "#switcher-small", function() {
 		$(".speech").css("font-size", "10px");
 	});
-
+	//On click change back to default
 	$(document).on("click", "#switcher-default", function() {
 		$(".speech").css("font-size", "13px");
 	});
-
+	//On mouse over the BG changes to yellow
 	$(document).on("mouseenter", "p", function() {
 		$(this).css("background-color", "yellow");
 	});
-
+	//On mouse leave it goes back to white
 	$(document).on("mouseleave", "p", function() {
 		$(this).css("background-color", "white");
 	});
-
+	//Custom animation to move the hearder and change the opacity
 	$(document).on("click", "#header", function() {
 		$("h2").animate({
             "margin-left": "+=20px", 
-            "color": "rgba(0, 0, 0, 0.25)"
-        }, 1000, "swing", function() {
-            $("#header").css("color", "rgba(0, 0, 0, 0.50)");
+            "opacity": ".25"
+        }, function() {
+            $(".speech").animate({
+            	"opacity": ".50"
+        	});
         });
     });
 
-    $(document).on("keypress","#switcher",function(e){
-    	
-    if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40)
-    console.log("Arrow key pressed");
-})
+
 
 
 
